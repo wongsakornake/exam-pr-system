9f03e539030d666656befefc4ff799f5d9ad68ca
LAPTOP-S96MC2IT\PIATEC-Dos <w.akenoppawut@piatec.co.th>
2026-02-18 19:13:19 +0700

    add URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
